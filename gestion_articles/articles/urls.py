3dee4b397c814596540d4da3e75a49e84f1152b1
MandePriscille <mandepriscille1@gmail.com>
2025-05-24 21:21:16 +0100

    Fix: implementation crud with ajax

--- gestion_articles/articles/urls.py
+++ gestion_articles/articles/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path('', views.index, name='index'),
+    path('create/', views.create_article, name='create_article'),
+    path('update/<int:id>/', views.update_article, name='update_article'),
+    path('delete/<int:id>/', views.delete_article, name='delete_article'),
+    path('detail/<int:id>/', views.get_article, name='get_article'),
 ]
--- gestion_articles/articles/views.py
+++ gestion_articles/articles/views.py
@@ -1,3 +1,69 @@
+import json
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Article
 
-# Create your views here.
+def index(request):
+    aticles = Article.objects.all()
+    return render(request, 'articles/index.html', {'articles': aticles})
+
+
+def create_article(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        article = Article.objects.create(
+            titre=['title'],
+            contenu=['content'],
+            auteur=['author'],
+        )
+        return JsonResponse({
+            'id': article.id,
+            'title': article.title,
+            'content': article.content,
+            'author': article.author,
+            'date': article.date_creation.strftime('%y/%m/%d'),
+        })
+    
+
+
+def update_article(request, id):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        try:
+            article = Article.object.get(id=id)
+            article.titre= data['title']
+            article.contenu = data['content']
+            article.auteur = data['author']
+            article.save()
+            return JsonResponse({
+                'id': article.id,
+                'title': article.title,
+                'content': article.content,
+                'author': article.author,
+            })
+        except Article.DoesNotExist:
+            return JsonResponse({'error': 'Article non trouve'}, status=404)
+        
+    
+def delete_article(request, id):
+    if request.method == 'POST':
+        try:
+            article = Article.object.get(id=id)
+            article.delete()
+            return JsonResponse({'message': 'Article supprime avec succes'})
+        except Article.DoesNotExist:
+            return JsonResponse({'error': 'Article non trouve'}, status=404)
+        
+
+def get_article(request, id):
+    try:
+        article = Article.object.get(id=id)
+        return JsonResponse({
+            'id': article.id,
+            'title': article.title,
+            'content': article.content,
+            'author': article.author,
+        })
+    except Article.DoesNotExist:
+        return JsonResponse({'error': 'Article non trouve'}, status=404)
+    
